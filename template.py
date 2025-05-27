f2f0e073f6d82a41af5dec67e8e925ba7db5c1f0
itsabhishekm <mabhishek2817@gmail.com>
2025-05-27 15:36:57 -0700

    Folder structure

---
+++ template.py
@@ -0,0 +1,53 @@
+#The main purpose of the template.py is to help setup the the file and folder structure required for a project
+#It is a reusable code for any project make sure to change this folder and file structure accordingly
+import os
+from pathlib import Path
+import logging
+
+#Printing the time and message
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+#Enter the project name
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",]
+
+
+for filepath in list_of_files:
+    #Changing the / to \ for windows requirement
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    #Once created the program won't overwrite the file unless the file is empty    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
